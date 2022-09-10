9bcd3774b9a6b3ec369e0b940c79d98c98a39aa2
Marti March <mmll1199@protonmail.com>
2022-09-10 14:45:07 +0200

    .

--- src/herramientas/Git.groovy
+++ src/herramientas/Git.groovy
@@ -11,7 +11,7 @@ class Git {
         this.steps.sh "rm -rf ./*"
         http_repo = http_repo.substring(7, http_repo.length())
         this.steps.withCredentials([this.steps.usernamePassword(credentialsId: 'GITHUB', passwordVariable: 'GIT_PASS', usernameVariable: 'GIT_USER')]){
-            this.steps.sh "git clone https://${GIT_USER}:${GIT_PASS}@" + http_repo
+            this.steps.sh "git clone https://${this.steps.GIT_USER}:${this.steps.GIT_PASS}@" + http_repo
         }
     }
 }
