ce0b11b5e0719f3bc166f32bd400a496e9be0d90
Marti March <mmll1199@protonmail.com>
2022-09-10 17:51:42 +0200

    .

--- src/herramientas/Helm.groovy
+++ src/herramientas/Helm.groovy
@@ -1,5 +1,32 @@
 package herramientas
 
 class Helm {
+    def steps
 
+    Helm(steps){
+        this.steps = steps
+    }
+
+    def template(){
+        steps.sh "helm template -f values.yaml ."
+    }
+
+    def lint(){
+        def out = steps.sh(returnStdout: true, script: "helm lint -f values.yaml .")
+        steps.echo "${out}"
+        if(!out.contains("0 chart(s) failed")){
+            steps.currentBuild.result = "FAILURE"
+            throw new Exception("Linted helm chart failed!!!")
+        }
+    }
+
+    def deploy(){
+        steps.withCredentials([steps.file(credentialsId: 'KUBECONFIG', variable: 'KUBECONFIG')]) {
+            steps.sh """
+                set +x
+                helm install formacion-jenkins . --kubeconfig ${steps.KUBECONFIG}
+                set -x
+            """
+        }
+    }
 }
--- vars/helm.groovy
+++ vars/helm.groovy
@@ -1,1 +1,16 @@
+import herramientas.Helm
 
+def template(steps){
+    Helm helm = new Helm(steps)
+    helm.template()
+}
+
+def lint(steps){
+    Helm helm = new Helm(steps)
+    helm.lint()
+}
+
+def deploy(steps){
+    Helm helm = new Helm(steps)
+    helm.deploy()
+}
